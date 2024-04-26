e1bcf9408a75d4cc1d8d776886d82b9e18599858
Yillié <yilliee@proton.me>
2024-04-26 22:38:40 +0500

    site : LandingPage : Create an initial LandingPageElement

--- site/components/LandingPage/LandingPage.jsx
+++ site/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,50 @@
-import Navbar from './Navbar/Navbar'
+import BackgroundImage from './assets/background.png'
+import SearchIcon from './assets/search_icon.jpg'
 
+import Navbar from './Navbar'
+// import LandingPageHeading from './LandingPageHeading'
 
 function LandingPage() {   
     return (
         <div>
             <Navbar />
-            <h1 className="text-3xl font-bold">Landing Page</h1>
+            <LandingElement />
         </div>
     )
 }
+
+function LandingElement() {
+    return (
+        <div className="w-screen h-screen text-white flex flex-col items-left justify-center px-8" style={{backgroundImage: `url(${BackgroundImage})`}}>
+            <div className="text-5xl font-bold">
+                <h1>
+                    Tech in trouble?
+                </h1>
+                <h1>
+                    Our trusted experts solve it fast
+                </h1>
+                <h1>
+                    Don&apos;t let glitches hold you back.
+                </h1>
+                <p className="text-lg font-normal mt-2">Get expert tech help now.</p>
+            </div>
+            <div className="flex items-center text-black">
+                <input
+                    className="mt-4 rounded-l-xl pl-2 py-1 h-12 w-96 border-black border-2 border-r-0"
+                    placeholder="What service do you need?"
+                />
+                <img
+                    className="mt-4 h-12 rounded-r-xl border-black border-2 border-l-0"
+                    src={SearchIcon}
+                />
+                <button className="h-12 w-32 mt-4 ml-5 rounded-xl px-3 py-1 bg-white border-black border-2">
+                    Search
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
 
 export default LandingPage;
---
+++ site/components/LandingPage/Navbar.jsx
@@ -0,0 +1,22 @@
+import Logo from '../../src/assets/react.svg'
+
+function LandingPageNavbar() {
+    return (
+        <nav className="w-screen flex items-center justify-between bg-black text-white p-4 font-bold">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center" onClick={() => {console.log("Navigate to Landing Page")}}>
+                    <img src={Logo} alt="Logo" className="h-8 w-8" />
+                    <h1 className="ml-2 text-xl">Tech - 911</h1>
+                </div>
+                <div className="flex items-center ml-4">
+                    <a href="/listings" className="mr-4">View All Listings</a>
+                    <a href="/tips" className="mr-4">Troubleshooting Tips</a>
+                    <a href="/track" className="mr-4">Track Order</a>
+                </div>
+            </div>
+            <button className="bg-white text-black rounded-lg px-4 py-2">Sign In</button>
+        </nav>
+    );
+}
+
+export default LandingPageNavbar;
