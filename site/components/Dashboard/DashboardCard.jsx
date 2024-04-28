7115ca2afe330d39a827bcdacaf104615e323e39
Yillié <yilliee@proton.me>
2024-04-28 16:58:00 +0500

    site : Dashboard : Create base card-like container

---
+++ site/components/Dashboard/DashboardCard.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import DashboardCardHeading from './DashboardCardHeading';
+import DashboardCardEntry from './DashboardCardEntry';
+
+function DashboardCard({TitleSize, TableHeading, TableEntries, MaxEntriesPerPage}) {
+    const [currentPage, setCurrentPage] = useState(0);
+    const PageCount = Math.ceil(TableEntries.length / MaxEntriesPerPage);
+    
+    function handlePageChange(pageNo) {
+        setCurrentPage(pageNo);
+    }
+    
+    const pageChangeButtons = Array.from({length: PageCount}, (_, idx) => {
+        return (
+            <button 
+                key={idx} onClick={() => handlePageChange(idx)}
+                className={`m-2 p-2 w-12 font-semibold rounded-2xl ${idx === currentPage ? 'bg-yellow-300 text-black' : 'bg-white text-black' }`}
+            >
+                {idx + 1}
+            </button>
+        );
+    });
+    return (
+        <div className="flex-inline flex-col justify-start mt-4 bg-black text-white rounded-3xl w-full">
+            <DashboardCardHeading TitleSize={TitleSize} Headings={TableHeading} />
+            {TableEntries.slice(currentPage*MaxEntriesPerPage, (currentPage + 1)*MaxEntriesPerPage).map(
+                    (entry, idx) => {
+                        return <DashboardCardEntry TitleSize={TitleSize} key={idx} EntryContent={entry}/>;
+                    }
+            )}
+            {PageCount > 1 &&
+                <div className="flex flex-row justify-center">
+                    {pageChangeButtons}
+                </div>
+            }
+        </div>
+    );
+}
+DashboardCard.propTypes = {
+    TitleSize: PropTypes.string,
+    TableHeading: PropTypes.arrayOf(PropTypes.string).isRequired,
+    TableEntries: PropTypes.array.isRequired,
+    MaxEntriesPerPage: PropTypes.number.isRequired,
+};
+DashboardCard.defaultProps = {
+    TitleSize: 'w-4/6',
+};
+
+export default DashboardCard;
---
+++ site/components/Dashboard/DashboardCardEntry.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+function DashboardCardEntry({TitleSize, EntryContent}) {
+    const headerClass = "flex flex-col truncate justify-center px-4 h-full text-center font-bold text-normal border-white";
+
+    console.log(TitleSize);
+
+    const elements = EntryContent.map((content, idx) => {
+        return (
+            <h3 key={idx} className={`${idx === 1 ? TitleSize : 'w-1/6'} ${headerClass} ${(idx + 1 < EntryContent.length) && 'border-r-2'}`}>{content}</h3>
+        );
+    });
+
+    return (
+        <div className="flex flex-row justify-center h-12 items-center border-b-2">
+           {elements}
+        </div>
+    )
+}
+DashboardCardEntry.propTypes = {
+    TitleSize: PropTypes.string.isRequired,
+    EntryContent: PropTypes.array.isRequired,
+};
+
+
+export default DashboardCardEntry;
