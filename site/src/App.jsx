38f57139987b3d31034b9fb21b2aaecb5d522f62
Yillié <yilliee@proton.me>
2024-05-27 18:07:18 +0500

    site : src : Implement React Router

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,12 +1,28 @@
-import LandingPage from '../components/LandingPage/LandingPage'
+import { RouterProvider, createBrowserRouter, useNavigate } from "react-router-dom";
+
+import LandingPage from "../components/LandingPage/LandingPage";
+import Login from '../components/Login/Login'
+import Register from '../components/Register/Register'
+import SearchPage from "../components/SearchPage/SearchPage"
+import ListingPage from "../components/Listings/ListingPage";
+
+import { useEffect } from "react";
+import { clearUserDetails } from "./utils";
 
 function App() {
+    const Routes=createBrowserRouter([
+        {path: "/", element: <LandingPage/>},
+        {path: "/signin", element: <Login/>},
+        {path: "/register", element: <Register />},
+        {path: "/search", element: <SearchPage/>},
+        {path: "/viewListing", element: <ListingPage />},
+    ])
+
     return (
-        <>
-            <LandingPage />
-        </>
+        <RouterProvider router={Routes}>
+            <LandingPage/>
+        </RouterProvider>
     );
 };
-
 
 export default App;
