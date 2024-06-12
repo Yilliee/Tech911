383fcb629d4b7c0caca0fa33b29e51752c235fc5
Yillié <yilliee@proton.me>
2024-06-12 22:53:54 +0500

    site : Listings : ListingPage : Fetch data from the API and pass data to helper functions accordingly

--- site/components/Listings/ListingPage.jsx
+++ site/components/Listings/ListingPage.jsx
@@ -1,38 +1,101 @@
-import laptopFan from './Data/laptopFan.jpeg'
-import laptopRam from './Data/laptopRam.jpeg'
-import laptopScreen from './Data/laptopScreen.jpeg'
-import reviews from '../Reviews/Data/TopReviewsData.js'
-
 import ListingPictureGrid from './ListingPictureGrid'
 import ListingPurchaseForm from './LisitingPurchaseForm'
 import ListingReviewsGrid from './ListingReviewsGrid'
 
 import NavBar from '../Navbar'
-import ListingReviews from './ListingReviewsGrid'
-
-const data = {
-    'title': 'Laptop Fan Replacement',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    'options': ['Option 1', 'Option 2', 'Option 3'],
-    'price': '$99.99',
-    'pictures': [   laptopFan, laptopRam, laptopScreen,
-                    laptopFan, laptopRam, laptopScreen,
-                    laptopFan, laptopRam, laptopScreen],
-}
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function ListingPage() {
-    const ListingData = data;
-    const isLogggedIn = true;
+    const location = useLocation();
 
-    document.title = ListingData.title;
+    const listingID = location.state?.listingID;
+    const [listingDetail, setListingDetail] = useState(null);
+    const [reviews, setReviews] = useState(null);
+
+    useEffect(() => {
+        if ( ! listingID || listingDetail !== null )
+            return;
+
+        fetch('http://localhost:3000/getListingDetails', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ listingID })
+        })
+        .then(response => response.json())
+        .then(data => {
+            setListingDetail(data);
+            document.title = data.title;
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error(error);
+        });
+    }, [listingID, listingDetail]);
+
+    useEffect(() => {
+        if ( ! listingID || reviews !== null )
+            return;
+
+        fetch('http://localhost:3000/getReviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ count: 10, listing_id: listingID })
+        })
+        .then(response => response.json())
+        .then(reviewData => {
+            const result = reviewData.map(review => (
+                {
+                    id: review.id,
+                    name: review.name,
+                    thumbnail: review.thumbnail ? URL.createObjectURL(new Blob( [new Uint8Array(review.thumbnail.data)], { type: 'image/jpeg' })) : "https://via.placeholder.com/150",
+                    rating: review.rating,
+                    description: review.description,
+                }
+            ));
+
+            setReviews(result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }, [listingID, reviews]);
+
+    if ( ! listingID ) {
+        return (
+            <div className="h-screen w-screen flex items-center justify-center text-center">
+                <h1 className="text-4xl font-bold text-red-700">Error: No listing ID provided</h1>
+            </div>
+        );
+    }
+    if ( ! listingDetail || ! reviews ) {
+        return (
+            <div className="h-screen w-screen flex items-center justify-center text-center">
+                <h1 className="text-4xl font-bold text-black">Loading...</h1>
+            </div>
+        );
+    }
+
+    const pictures = listingDetail.pictures.map((picture) => ({
+        'id': picture.picture_id,
+        'picture': URL.createObjectURL(new Blob( [new Uint8Array(picture.picture.data)], { type: 'image/jpeg' })),
+    }));
+    const thumbnailIndex = pictures.findIndex((picture) => picture.id === listingDetail.thumbnail_id);
+
     return (
         <div className="w-full h-full">
             <NavBar showAccountButtons={false} showSearchBar={true} />
-            <div className="flex flex-row items-center justify-between">
-                <ListingPictureGrid imagesArr={ListingData.pictures}/>
-                <ListingPurchaseForm Data={ListingData} loggedIn={isLogggedIn}/>
+            <div className='flex flex-col items-start justify-between p-8'>
+                <div className="flex flex-row items-center justify-between">
+                    <ListingPictureGrid imagesArr={pictures} thubmnailIndex={thumbnailIndex}/>
+                    <ListingPurchaseForm listing={listingDetail}/>
+                </div>
+                {reviews.length > 0 && <ListingReviewsGrid reviews={reviews} />}
             </div>
-            <ListingReviewsGrid reviews={reviews} />
         </div>
     );
 }
--- site/components/Listings/ListingPictureGrid.jsx
+++ site/components/Listings/ListingPictureGrid.jsx
@@ -2,38 +2,37 @@ import { useState } from 'react';
 import PropTypes from 'prop-types'
 import arrowPic from './assets/arrow.png'
 
-function ListingPictureGrid({imagesArr}) {
+function ListingPictureGrid({imagesArr, thubmnailIndex = 0}) {
     // One pic is reserved for thubmnail
-    const picturesCount = imagesArr.length - 1;
+    const picturesCount = imagesArr.length;
     const maxPicturesAtOnce = 4;
 
     const [pictureListIndex, setPictureListIndex] = useState(0);
-    const [mainPictureIndex, setMainPictureIndex] = useState(0);
+    const [mainPictureIndex, setMainPictureIndex] = useState(thubmnailIndex);
 
     function updatePicturesIndex(increment) {
         let newIndex = pictureListIndex + increment;
         if (newIndex < 0) newIndex = picturesCount - 1;
-
 
         setPictureListIndex(newIndex % picturesCount);
     }
 
     return (
         <div className="flex flex-col ml-12 mt-16 w-5/12">
-            <img src={imagesArr[mainPictureIndex]} className="w-full rounded-3xl" />
+            <img src={imagesArr[mainPictureIndex].picture} className="w-full rounded-3xl aspect-16/10" />
             <div className="w-full mt-4 flex flex-row items-center justify-evenly">
                 {picturesCount > maxPicturesAtOnce &&
                     <img
                         src={arrowPic}
                         className="w-16 rounded-full rotate-180"
                         onClick={() => updatePicturesIndex(-1)} />}
-                {imagesArr.slice(pictureListIndex, Math.min(pictureListIndex + maxPicturesAtOnce, picturesCount)).map((img, index) => (
-                    <img key={index} src={img}
+                {imagesArr.slice(pictureListIndex, Math.min(pictureListIndex + maxPicturesAtOnce, picturesCount)).map(({id, picture}, index) => (
+                    <img key={id} src={picture}
                         className="w-2/12 mx-4 rounded-lg"
                         onClick={() => setMainPictureIndex(pictureListIndex + index)} />
                 ))}
-                {imagesArr.slice(0, Math.max(0, pictureListIndex + maxPicturesAtOnce - picturesCount)).map((img, index) => (
-                    <img key={index} src={img}
+                {picturesCount > maxPicturesAtOnce && imagesArr.slice(0, Math.max(0, pictureListIndex + maxPicturesAtOnce - picturesCount)).map(({id, picture}, index) => (
+                    <img key={id} src={picture}
                         className="w-2/12 mx-4 rounded-lg"
                         onClick={() => setMainPictureIndex(index)} />
                 ))}
@@ -49,6 +48,7 @@ function ListingPictureGrid({imagesArr}) {
 
 ListingPictureGrid.propTypes = {
     imagesArr: PropTypes.array.isRequired,
+    thubmnailIndex: PropTypes.number,
 };
 
 export default ListingPictureGrid;
